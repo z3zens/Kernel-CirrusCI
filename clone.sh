1fe44d0c6eef2db54017a691adb996f5bfddf5e3
z3zens <78209717+z3zens@users.noreply.github.com>
2022-11-16 06:07:47 +0700

    clone.sh: Don't using function [1/2] [skip ci]

--- clone.sh
+++ clone.sh
@@ -6,17 +6,11 @@
 # Based on Panchajanya1999 script.
 #
 
-# Set function for cloning repository
-clone() {
-	echo " "
-	# Clone AnyKernel3
-	git clone --depth=1 https://github.com/nerdprojectorg/AnyKernel3.git -b master AnyKernel3
+# Clone AnyKernel3
+git clone --depth=1 https://github.com/nerdprojectorg/AnyKernel3.git -b master AnyKernel3
 
-	# Clone Clang
-	git clone --depth=1 https://gitlab.com/RyuujiX/neutron-clang -b Neutron-16 clang
+# Clone Clang
+git clone --depth=1 https://gitlab.com/RyuujiX/neutron-clang -b Neutron-16 clang
     
-        # Clone Kernel
-        git clone --depth=1 https://github.com/nerdprojectorg/android_kernel_asus_sdm660 -b tm kernel
-}
-
-clone
+# Clone Kernel
+git clone --depth=1 https://github.com/nerdprojectorg/android_kernel_asus_sdm660 -b tm kernel
