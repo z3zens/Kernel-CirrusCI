755dc5ce08fd116dab7dafb4ccfe9f259cec9f78
z3zens <78209717+z3zens@users.noreply.github.com>
2022-11-16 06:08:54 +0700

    upload.sh: Don't using function [2/2]

--- upload.sh
+++ upload.sh
@@ -6,27 +6,12 @@
 # Based on Panchajanya1999 script.
 #
 
-# Function to show an informational message
-msg() {
-    echo -e "\e[1;32m$*\e[0m"
-}
+mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+cd AnyKernel3 || exit
+zip -r9 "$ZIPNAME" * -x .git README.md
 
-err() {
-    echo -e "\e[1;41m$*\e[0m"
-    exit 1
-}
+# Prepare a final zip variable
+ZIP_FINAL="$ZIPNAME"
 
-# Set function for zipping into a flashable zip
-gen_zip() {
-	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
-	cd AnyKernel3 || exit
-	zip -r9 "$ZIPNAME" * -x .git README.md
-
-	# Prepare a final zip variable
-	ZIP_FINAL="$ZIPNAME"
-
-	tg_post_build "$ZIP_FINAL" "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
-	cd ..
-}
-
-gen_zip
+tg_post_build "$ZIP_FINAL" "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+cd ..
